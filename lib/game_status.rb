c353ae67f1fede6e2e17e79c707e3a112a7ea5d1
binding-proc-8938 <naomirhames2020@gmail.com>
2020-11-14 02:07:32 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,53 @@
 # Helper Method
+require "pry"
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], 
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2],
+]
+def won?(board)
+        WIN_COMBINATIONS.each do |win_combination|
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
+     
+      position_1 = board[win_index_1] 
+      position_2 = board[win_index_2] 
+      position_3 = board[win_index_3] 
+    
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"
+        return win_combination # return the win_combination indexes that won.
+        elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+        return win_combination
+      else
+        false
+      end
+    end
+    false
+  end
+
+ def full?(board) 
+  board.all?{ |space| 
+    space == "X" || space ==  "O" 
+  }
+end
+def draw?(board)
+  full?(board) && !won?(board)
+ end
+ def over?(board)
+   full?(board) || won?(board)
+ end
+def winner(board)
+  
+end
+    
+    
